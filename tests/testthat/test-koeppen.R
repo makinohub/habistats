e9799663a01e57bc4b873e19a67a75802debe140
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-12-04 16:14:37 +0900

    test: Use expect_shape() from testthat 3.3

--- tests/testthat/test-koeppen.R
+++ tests/testthat/test-koeppen.R
@@ -1,8 +1,8 @@
 test_that("raster_kgc works", {
-  x = raster_kgc()
-  expect_s3_class(x, "stars")
-  expect_length(x, 1L)
-  expect_identical(dim(x), c(x = 4320L, y = 2160L))
+  x = raster_kgc() |>
+    expect_s3_class("stars") |>
+    expect_length(1L) |>
+    expect_shape(dim = c(x = 4320L, y = 2160L))
   expect_s3_class(x[[1L]], "factor")
   expect_length(levels(x[[1L]]), 32L)
 })
